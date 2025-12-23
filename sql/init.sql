e6d459c066fe2b551e04451a5aaaa9acf1062a05
JosephRer <jrechdiner2002@gmail.com>
2025-12-23 12:00:22 +0200

    added db init file

---
+++ sql/init.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE database;
+
+CREATE TABLE contacts(
+  Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  First_name VARCHAR(50) NOT NULL,
+  Last_name VARCHAR(50) NOT NULL,
+  Phone_number VARCHAR(20) NOT NULL UNIQUE
+);
+
+INSERT INTO contacts(First_name, Last_name, Phone_number)
+VALUES ("Yoseph", "Rechdiener", "054-2985519")
+       ("David", "Rechdiener", "054-2986210")
+       ("Talya", "Rechdiener", "054-2980112");
+
