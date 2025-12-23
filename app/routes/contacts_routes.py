e7879e895839ead3be489a87f46f6bcbee1cdd4f
JosephRer <jrechdiner2002@gmail.com>
2025-12-23 18:04:42 +0200

    added some prefixes for work

---
+++ app/routes/contacts_routes.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, Depends
+from database.create_connection import get_connection
+from mysql.connector import connection
+
+contact_router = APIRouter(prefix="/contanct")
+
+@contact_router.get("/")
+def home(conn: connection = Depends(get_connection)):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM contacts; ")
+    rows = cursor.fetchall()
+    data = []
+    for row in rows:
+        cur_row = {}
+        cur_row["First name"] = row[0]
+        cur_row["Last name"] = row[1]
+        cur_row["Phone number"] = row[2]
+        
+        data.append(cur_row)
+    cursor.close()
+    conn.close()
+    return {"data": data}
