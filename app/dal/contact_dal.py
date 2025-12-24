832b4f887b58246628c76a34aa19ced97ce91431
JosephRer <jrechdiner2002@gmail.com>
2025-12-24 11:57:15 +0200

    changed to manager classes

--- app/dal/contact_dal.py
+++ app/dal/contact_dal.py
@@ -1,83 +1,83 @@
 from mysql.connector import connection
 from utils import convert_db_to_objects
 
+class DalManager:
+    def get_all_contacts(conn: connection):
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM contacts;")
+        rows = cursor.fetchall()
 
-def get_all_contacts(conn: connection):
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM contacts;")
-    rows = cursor.fetchall()
+        if not rows:
+            return False 
 
-    if not rows:
-        return False 
-
-    data = convert_db_to_objects(rows) 
-    cursor.close()
-    conn.close()
-    return data
+        data = convert_db_to_objects(rows) 
+        cursor.close()
+        conn.close()
+        return data
 
 
-def create_contact(first_name: str, last_name: str, phone_number: str, conn: connection):
-    cursor = conn.cursor()
-    try: 
-        cursor.execute(
-            "INSERT INTO contacts (first_name, last_name, phone_number) " \
-            "VALUES(%s, %s, %s);",(
-                first_name,
-                last_name,
-                phone_number,
+    def create_contact(first_name: str, last_name: str, phone_number: str, conn: connection):
+        cursor = conn.cursor()
+        try: 
+            cursor.execute(
+                "INSERT INTO contacts (first_name, last_name, phone_number) " \
+                "VALUES(%s, %s, %s);",(
+                    first_name,
+                    last_name,
+                    phone_number,
+                    )
                 )
-            )
-        contact_id = cursor.lastrowid
-        conn.commit()
-        return contact_id
-    
-    except Exception:
-        conn.rollback()
-        raise
-    finally:
-        cursor.close()
+            contact_id = cursor.lastrowid
+            conn.commit()
+            return contact_id
+        
+        except Exception:
+            conn.rollback()
+            raise
+        finally:
+            cursor.close()
 
 
-def update_contact(id: int, first_name: str, last_name: str, phone_number: str, conn: connection):
-    cursor = conn.cursor()
-    try:
-        cursor.execute("SELECT * FROM contacts WHERE id = %s;", (id,))
-        user_to_update = cursor.fetchone()
-        if not user_to_update:
-            return False
+    def update_contact(id: int, first_name: str, last_name: str, phone_number: str, conn: connection):
+        cursor = conn.cursor()
+        try:
+            cursor.execute("SELECT * FROM contacts WHERE id = %s;", (id,))
+            user_to_update = cursor.fetchone()
+            if not user_to_update:
+                return False
 
-        cursor.execute(
-        "UPDATE contacts " \
-        "SET first_name = %s, last_name = %s, phone_number = %s WHERE id = %s;",(
-                first_name,
-                last_name,
-                phone_number,
-                id)
-            )
-        conn.commit()
-        return True
+            cursor.execute(
+            "UPDATE contacts " \
+            "SET first_name = %s, last_name = %s, phone_number = %s WHERE id = %s;",(
+                    first_name,
+                    last_name,
+                    phone_number,
+                    id)
+                )
+            conn.commit()
+            return True
 
-    except Exception:
-        conn.rollback()
-        raise
-    finally:
-        cursor.close()
+        except Exception:
+            conn.rollback()
+            raise
+        finally:
+            cursor.close()
 
 
-def delete_contact(id: int, conn: connection):
-    cursor = conn.cursor()
-    try:
-        cursor.execute("SELECT * FROM contacts WHERE id = %s;", (id,))
-        user_to_delete = cursor.fetchone()
-        if not user_to_delete:
-            return False
+    def delete_contact(id: int, conn: connection):
+        cursor = conn.cursor()
+        try:
+            cursor.execute("SELECT * FROM contacts WHERE id = %s;", (id,))
+            user_to_delete = cursor.fetchone()
+            if not user_to_delete:
+                return False
 
-        cursor.execute("DELETE FROM contacts WHERE id = %s;",(id,))
-        conn.commit()
-        return True
+            cursor.execute("DELETE FROM contacts WHERE id = %s;",(id,))
+            conn.commit()
+            return True
 
-    except Exception:
-        conn.rollback()
-        raise
-    finally:
-        cursor.close()
+        except Exception:
+            conn.rollback()
+            raise
+        finally:
+            cursor.close()
--- app/routes/contacts_routes.py
+++ app/routes/contacts_routes.py
@@ -1,24 +1,23 @@
 from fastapi import APIRouter, Depends
 from mysql.connector import connection
 from database.create_connection import get_connection
-from service import contact_service
+from service.contact_service import ServiceManager
 from routes.schemas import InputContact
-from database.contact_model import Contact
 
 contact_router = APIRouter(prefix="/contancts")
 
 @contact_router.get("/get_contacts")
 def get_all_contacts(conn: connection = Depends(get_connection)):
-    return contact_service.list_contacts(conn)
+    return ServiceManager.list_contacts(conn)
 
 @contact_router.post("/add_contact")
 def add_one_contact(contact: InputContact, conn: connection = Depends(get_connection)):
-    return contact_service.add_contact(contact.first_name, contact.last_name, contact.phone_number, conn)
+    return ServiceManager.add_contact(contact.first_name, contact.last_name, contact.phone_number, conn)
 
 @contact_router.put("/update_contact{id}")
 def update_contact_by_id(id: int, contact: InputContact, conn: connection = Depends(get_connection)):
-    return contact_service.update_contact_info(id, contact.first_name, contact.last_name, contact.phone_number, conn)
+    return ServiceManager.update_contact_info(id, contact.first_name, contact.last_name, contact.phone_number, conn)
 
 @contact_router.delete("/delete_contact/{id}")
 def delete_contact_by_id(id: int, conn: connection = Depends(get_connection)):
-    return contact_service.remove_contact(id, conn)
+    return ServiceManager.remove_contact(id, conn)
