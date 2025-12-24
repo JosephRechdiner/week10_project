499adf96f7a9c8db7e8dd08a816e40c801895cfb
JosephRer <jrechdiner2002@gmail.com>
2025-12-24 10:30:10 +0200

    removed func from utils

--- app/utils.py
+++ app/utils.py
@@ -1,12 +1,4 @@
 from database.contact_model import Contact
-
-def load_sql(path):
-    try:
-        with open(path, "r") as file:
-            file_script = file.read()
-            return file_script
-    except Exception as e:
-        return {"Error": e}
     
 def convert_db_to_objects(rows):
     objects = []
